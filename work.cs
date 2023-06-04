17b63fa2dccb3fa526e7ef6faa9ed828c13e14cf
Your Name <vanch.katolik@mail.ru>
2023-06-05 00:13:35 +0300

    Tree work

---
+++ work.cs
@@ -0,0 +1,16 @@
+﻿var i = Console.ReadLine();
+switch(i) 
+{
+ case("w"):
+ Console.WriteLine("Стоят как-то два барана в поле. Один такой:бееее. А второй ему отвечает: б*я, тоже самое хотел сказать");
+ break;
+  case("a"):
+ Console.WriteLine("Приходит русская бабка в Америке в магазин и говорит продавцу авроамериканцу: Дай манки");
+ break;
+  case("s"):
+ Console.WriteLine("Спасибо за лекции");
+ break;
+  case("d"):
+ Console.WriteLine("Расскажите какой-то анекдот");
+ break;
+}
